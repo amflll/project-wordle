00b79fc4f914b952641a9a467f3a0fea07e2a719
amflll <amelia.fong@live.com>
2025-02-21 21:19:15 +0800

    exercise one GuessInput

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -9,7 +9,29 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [word, setWord] = React.useState("");
+
+  return <>
+  <form 
+    className="guess-input-wrapper"
+    onSubmit={(event) => {
+      event.preventDefault();
+      console.log(word);
+      setWord('');
+    }}
+    >
+    <label for="guess-input">Enter guess:</label>
+  <input 
+    id="guess-input" 
+    type="text" 
+    pattern="[A-Z]{5}" //pattern attribute, 5 characters and must be uppercase 
+    value={word}
+          onChange={event => {
+            setWord((event.target.value).toUpperCase());
+          }}
+    />
+</form>
+  </>;
 }
 
 export default Game;
